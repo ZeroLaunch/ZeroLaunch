7e46f07a4140c79bda50f56467c31eebd6db8472
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2026-01-07 02:11:56 +0330

    thinking for an abi/api

--- example/zerolaunch.py
+++ example/zerolaunch.py
@@ -1,1 +1,12 @@
 import zerolaunch
+
+zerolaunch = new zerolaunch()
+
+structure_post = new structure()
+structure_post.add_field("id")
+structure_post.add_field("title")
+structure_post.add_field("description")
+structure_post.add_field("create_at")
+
+zerolaunch.add_structures(structure_post)
+zerolaunch.port = 3000
